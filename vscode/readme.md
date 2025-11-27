758b0829fceaa6116a6241d61d9e91bd9936e8a7
Ts-sound <1564264931@qq.com>
2025-11-27 10:24:30 +0000

    Add new features and update configurations for Python and C++ extensions in VSCode

--- vscode/readme.md
+++ vscode/readme.md
@@ -1,5 +1,10 @@
 # vscode
 
+* [√] 查看所以已安装组件
+* [√] 查看指定 group 安装信息(已安装，版本有变化,未安装)
+* [√] 安装指定 group 组件列表 (-f 强制更新版本不一致组件)
+* [-] 安装指定 extension 组件 (-f 强制更新版本不一致组件)
+
 ## vscode list extension
 
 ```bash
--- vscode/scripts/install_extensions.py
+++ vscode/scripts/install_extensions.py
@@ -4,7 +4,7 @@ from pathlib import Path
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s - %(levelname)s %(funcName)s:%(lineno)d]: %(message)s")
 
 filepath = str(Path(__file__).resolve().parent)
-logging.info(f"workpath: {filepath}")
+logging.debug(f"workpath: {filepath}")
 
 class ExtensionInfo:
     def __init__(self, name:str):
@@ -21,6 +21,7 @@ class ExtensionInfo:
 
     def __repr__(self):
         return f"{self.vendor}.{self.name}@{self.version}" if self.version else f'{self.vendor}.{self.name}'
+    
 
 def GetInstalledExtensionsList() -> list:
     '''
@@ -38,8 +39,8 @@ def GetInstalledExtensionsList() -> list:
         if match:
             valid_extensions.append(ext)
         else:
-            logging.warning(f"Invalid extension format: {ext}")
-    logging.info(f"Installed extensions: {valid_extensions}")
+            logging.debug(f"warning: Invalid extension format: {ext}")
+    logging.debug(f"Installed extensions: {valid_extensions}")
     
     return valid_extensions
 
@@ -49,22 +50,26 @@ class ExtensionInstaller:
         self.installed_extensions = [ExtensionInfo(ext) for ext in GetInstalledExtensionsList()]
         self.extensions_files = ExtensionInstaller.list_extensions_files()
         # dict {file_shortname: (file,[extensions])}
-        self.extensions_file_context_dict = [{  Path(file).stem : (file,ExtensionInstaller.list_extensions_file_context(file))} for file in self.extensions_files]
-        logging.info(f"Extensions files context dict: {self.extensions_file_context_dict}")
+        self.extensions_file_context_dict = {Path(file).stem: (file, ExtensionInstaller.list_extensions_file_context(file)) for file in self.extensions_files}
+        logging.debug(f"Extensions files context dict: {self.extensions_file_context_dict}")
         
-    def is_installed(self, extension:ExtensionInfo) -> bool:
-        ret = [ext for ext in self.installed_extensions if ext.equals(extension, is_compare_version=True)]
+    def is_installed(self, extension:ExtensionInfo,is_compare_version=True) -> bool:
+        ret = [ext for ext in self.installed_extensions if ext.equals(extension, is_compare_version=is_compare_version)]
         return len(ret) > 0
+    
+    def get_installed_extension(self, extension:ExtensionInfo,is_compare_version=True) -> ExtensionInfo:
+        ret = [ext for ext in self.installed_extensions if ext.equals(extension, is_compare_version=is_compare_version)]
+        return ret[0] if len(ret) > 0 else None
     
     def install_extension(self, extension:ExtensionInfo):
         if self.is_installed(extension):
-            logging.info(f"Extension {extension} is already installed.")
+            logging.debug(f"Extension {extension} is already installed.")
             return
         
         cmd = f"code --install-extension {str(extension)}"
-        logging.info(f"Installing extension: {extension} with command: {cmd}")
+        logging.info(f"Installing extension:  with command: {cmd}")
         result = os.popen(cmd).read()
-        logging.info(f"Installation result: {result}")
+        logging.info(f"result: {result}")
         
     def list_extensions_files() -> list:
         '''
@@ -73,7 +78,7 @@ class ExtensionInstaller:
         files = glob.glob(os.path.join(filepath, '../extensions/*.txt'))
         # return absolute paths
         files = [os.path.abspath(f) for f in files]
-        logging.info(f"Found extension files: {files}")
+        logging.debug(f"Found extension files: {files}")
         return files
     
     def list_extensions_file_context(file:str):
@@ -88,5 +93,90 @@ class ExtensionInstaller:
             lines = f.readlines()
         
         extensions = [line.strip() for line in lines if line.strip() and not line.startswith('#')]
-        logging.info(f"Extensions in file {file}: {extensions}")
+        logging.debug(f"Extensions in file {file}: {extensions}")
         return extensions
+
+###
+class GroupExtensionsInstallDetail:
+    def __init__(self, installer:ExtensionInstaller, group:str):
+        path,group_exts = installer.extensions_file_context_dict[args.group]
+        
+        # installed and version compatible
+        self.install_exts=[ext for ext in group_exts if installer.is_installed(ExtensionInfo(ext))]
+        
+        # installed but version not compatible
+        self.install_exts_version_not_compatible = [ExtensionInfo(ext) for ext in group_exts if installer.is_installed(ExtensionInfo(ext),is_compare_version=False) and  ext not in self.install_exts]
+        
+        # not installed
+        self.not_install_exts = [ext for ext in group_exts if not installer.is_installed(ExtensionInfo(ext),is_compare_version=False)]
+
+#####
+
+
+def handle_list_installed_with_group(installer:ExtensionInstaller, group:str):
+    group_detail = GroupExtensionsInstallDetail(installer,group)
+    
+    logging.info(f"{group} Installed Extensions --- :] ")
+    for ext in group_detail.install_exts:
+        logging.info(f"{ext}")
+    
+    logging.info(f"\n{group} Installed Extensions with different version --- :] ")
+    for ext in group_detail.install_exts_version_not_compatible:
+        logging.info(f"installed: [{installer.get_installed_extension(ext,False)}], wanted version: [{ext.version}]")
+
+    logging.info(f"\n{group} Not Installed Extensions --- :] ")
+    for ext in group_detail.not_install_exts:
+        logging.info(f"{ext}")
+    return
+
+def handle_install_target_group(installer:ExtensionInstaller,install_pattern:str, group:str, force:bool=False):
+    group_detail = GroupExtensionsInstallDetail(installer,group)
+    
+    # install not installed extensions
+    for ext in group_detail.not_install_exts:
+        if(install_pattern=="*" or ext==install_pattern):
+            installer.install_extension(ExtensionInfo(ext))
+    
+    # force reinstall extensions with different version
+    if(force):
+        for ext in group_detail.install_exts_version_not_compatible:
+            installer.install_extension(ext)
+    return
+
+def main(args=None):
+    
+    logging.basicConfig(level=logging.INFO, format="%(message)s",force=True)
+    
+    # args empty
+    if args is None:
+        argparse.print_help()
+        return
+    
+    # args
+    list_installed = args.list_installed
+    group = args.group
+    install = args.install
+   
+    if(list_installed and not group):
+        extensions = GetInstalledExtensionsList()
+        logging.info("Installed Extensions --- :] ")
+        for ext in extensions:
+            logging.info(f"{ext}")
+        return
+    
+    installer = ExtensionInstaller()
+    
+    if(list_installed and group):
+        handle_list_installed_with_group(installer, group)
+    elif(install and group):
+        handle_install_target_group(installer,install, group, force= (args.force  if args.force is None else False))
+    return
+
+if __name__ == '__main__':
+    argparse = argparse.ArgumentParser(description="VSCode Extension Installer Script")
+    argparse.add_argument('--install','-i', type=str, help="Install the specified extension (format: vendor.name@version) , * for all")
+    argparse.add_argument('--group','-g', type=str, help="all,cpp,python,rust ..., group name to install extensions from  extensions/[group].txt")
+    argparse.add_argument('--list-installed','-l', action='store_true', help="List all installed extensions")
+    argparse.add_argument('--force','-f', type=bool, help="Force reinstall even if the extension is already installed")
+    args = argparse.parse_args()
+    main(args)
