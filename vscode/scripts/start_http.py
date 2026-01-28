87985b253ddde19ee2cf7b6051224faf235b694f
Ts-sound <1564264931@qq.com>
2026-01-28 02:25:55 +0000

    Remove Cache-Control header from CORSRequestHandler in start_http.py

--- vscode/scripts/start_http.py
+++ vscode/scripts/start_http.py
@@ -15,7 +15,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET")
-        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
+        # self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
         return super().end_headers()
 
 
